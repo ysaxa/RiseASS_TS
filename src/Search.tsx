97eea864884fe7b83f05824b4cf5c60d2d5e7bae
ysaxa <102462519+ysaxa@users.noreply.github.com>
2023-04-30 19:47:19 +0200

    implement filterArmor

--- src/Search.tsx
+++ src/Search.tsx
@@ -18,8 +18,46 @@ class Results {
 }
 
 function filterArmor(wantedSkills: string[]): ArmorPiece[] {
-	// TODO actually should also sometimes return armor pieces with irrelevant skills BUT with top skill slots available
-	return armor.filter(a=>Object.keys(a.skills).some(sa=>wantedSkills.some(sw=>sw===sa)))
+	function isStrictlyBetter(champion: ArmorPiece, candidate: ArmorPiece): boolean {
+		// [4] isn't strictly better than [1,1]
+		// [4,1] isn't strictly better than [2,2]
+		// [4] is strictly better than [3] which is strictly better than [2]
+		// [4,1] is strictly better than [4] and than [3,1]
+		// so basically [...W] is worse than [...B] if we have those two conditions
+		// - [...W].length <= [...B].length
+		// - when ordered, every value in W is less than the value in B at the same index
+
+		if (candidate.slotSizes.length > champion.slotSizes.length) return false
+
+		let orderedChampion: number[] = [...champion.slotSizes].sort()
+		let orderedCandidate: number[] = [...candidate.slotSizes].sort()
+		return [...Array(orderedCandidate.length).keys()].every(i => orderedChampion[i] >= orderedCandidate[i])
+	}
+
+	// armor pieces that contribute to the wanted skills
+	let directlyRelevant: ArmorPiece[] = armor.filter(a=>Object.keys(a.skills).some(sa=>wantedSkills.some(sw=>sw===sa)))
+
+	// armor pieces with the best deco slots, even if they don't contribute to the wanted skills
+	let bestDecoSlots: ArmorPiece[] = []
+	;[...new Set(armor.map(a=>a.place))].forEach(place=>{
+		let bestDecoSlotsLocal: ArmorPiece[] = []
+		armor.filter(a=>a.place===place).forEach(candidate => {
+			// obvious edgecase
+			if (bestDecoSlotsLocal.length === 0) {
+				bestDecoSlotsLocal = bestDecoSlotsLocal.concat(candidate)
+				return
+			}
+
+			// if any of the current bests is strictly better than the candidate, forget the candidate
+			if (bestDecoSlotsLocal.some(best => isStrictlyBetter(best, candidate))) return
+			// otherwise, keep the candidate and remove any current best that is actually worse
+			bestDecoSlotsLocal = bestDecoSlotsLocal.filter(best => !isStrictlyBetter(candidate, best))
+			bestDecoSlotsLocal = bestDecoSlotsLocal.concat(candidate)
+		})
+		bestDecoSlotsLocal.forEach(a=>bestDecoSlots = bestDecoSlots.concat(a))
+	})
+
+	return [...directlyRelevant, ...bestDecoSlots]
 }
 
 function search(wantedSkills: { [skill: string]: number }, results: Results): void {
